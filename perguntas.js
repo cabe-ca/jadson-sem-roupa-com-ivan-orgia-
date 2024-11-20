53cb805d1b6e7b8ed2aff6bf0978c713947f7344
cabe-ca <vitorhugogasparini2007@gmail.com>
2024-11-19 22:18:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futbol',
+    'Maior artilheiro da champions?',
+    'CR7'
+)
+
+criaCartao(
+    'orgia',
+    'quem mais faz?',
+    'Ivan'
+)
+
+criaCartao(
+    'dadoi',
+    'tem dado em casa?',
+    'a vezes'
+)
+
+criaCartao(
+    'marca',
+    'pior plataforma?',
+    'Speak'
+)
